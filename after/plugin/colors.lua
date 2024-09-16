b39c040207bf33b8698e13ae7d7bb49e58f7c7ae
ngdangdat <ngdangdat09@gmail.com>
2024-09-16 20:48:49 +0900

    feat: change to use flow.nvim

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,4 @@
-local  color = color or "rose-pine"
+local  color = color or "flow"
 vim.cmd.colorscheme(color)
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/ngdangdat/packer.lua
+++ lua/ngdangdat/packer.lua
@@ -10,13 +10,11 @@ return require('packer').startup(function(use)
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-    end })
-
+  use ('0xstepit/flow.nvim', {
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  })
   use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use 'nvim-treesitter/playground' 
   use 'ThePrimeagen/harpoon'
