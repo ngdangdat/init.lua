03332086dfc0a35e7477091157ff5c5e2573f55a
ngdangdat <ngdangdat09@gmail.com>
2026-03-04 17:03:33 +0900

    chore: set undofile to true

--- lua/customs/config.lua
+++ lua/customs/config.lua
@@ -13,6 +13,7 @@ vim.opt.smarttab = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function() 
